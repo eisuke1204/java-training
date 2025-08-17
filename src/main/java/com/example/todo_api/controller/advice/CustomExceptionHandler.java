75d7a2be081a90756a3ed2b635506026350e81a7
eisuke <h6rsb4ke@gmail.com>
2025-08-17 15:08:57 +0900

    タスク一覧取得の実装

--- src/main/java/com/example/todo_api/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todo_api/controller/advice/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package com.example.todo_api.controller.advice;
 import com.example.todo_api.service.task.TaskEntityNotFoundException;
 import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -31,6 +32,14 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
             HttpStatusCode status,
             WebRequest request
     ) {
+        var error = BadRequestErrorCreator.from(ex);
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<BadRequestError> handlerConstraintViolationException(
+            ConstraintViolationException ex
+    ){
         var error = BadRequestErrorCreator.from(ex);
         return ResponseEntity.badRequest().body(error);
     }
--- src/main/java/com/example/todo_api/controller/task/TaskController.java
+++ src/main/java/com/example/todo_api/controller/task/TaskController.java
@@ -1,9 +1,12 @@
 package com.example.todo_api.controller.task;
 
+import com.example.todo_api.service.task.TaskEntity;
 import com.example.todo_api.service.task.TaskService;
 import com.example.todoapi.controller.TasksApi;
+import com.example.todoapi.model.PageDTO;
 import com.example.todoapi.model.TaskDTO;
 import com.example.todoapi.model.TaskForm;
+import com.example.todoapi.model.TaskListDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,21 +27,44 @@ public class TaskController implements TasksApi {
     @Override
     public ResponseEntity<TaskDTO> showTask(Long taskId) {
         var entity = taskService.find(taskId);
-        var dto = new TaskDTO();
-        dto.setId(entity.getId());
-        dto.setTitle(entity.getTitle());
+        var dto = toTaskDTO(entity);
         return ResponseEntity.ok(dto);
     }
 
     @Override
     public ResponseEntity<TaskDTO> createTask(@Valid @RequestBody TaskForm form) {
         var entity = taskService.create(form.getTitle());
-        var dto = new TaskDTO();
-        dto.setId(entity.getId());
-        dto.setTitle(entity.getTitle());
+        var dto = toTaskDTO(entity);
 
         return ResponseEntity
                 .created(URI.create("/tasks/" + dto.getId()))
                 .body(dto);
+    }
+
+
+    @Override
+    public ResponseEntity<TaskListDTO> listTasks(Integer limit, Long offset) {
+        var entityList = taskService.find(limit, offset);
+        var dtoList = entityList.stream()
+                .map(this::toTaskDTO)
+                .collect(Collectors.toList());
+
+        var pageDTO = new PageDTO();
+        pageDTO.setLimit(limit);
+        pageDTO.setOffset(offset);
+        pageDTO.setSize(dtoList.size());
+
+        var dto = new TaskListDTO();
+        dto.setPage(pageDTO);
+        dto.setResults(dtoList);
+
+        return ResponseEntity.ok(dto);
+    }
+
+    private TaskDTO toTaskDTO(TaskEntity taskEntity) {
+        var taskDTO = new TaskDTO();
+        taskDTO.setId(taskEntity.getId());
+        taskDTO.setTitle(taskEntity.getTitle());
+        return taskDTO;
     }
 }
